8097467ef7159501886ce76dcc7b54b64858c104
Yuliya <cudina.iu@yandex.ru>
2024-06-01 22:06:12 +0300

    Article

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,18 +1,24 @@
 import { useLocation, useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import { Article } from '../features/Article/ui/Article';
 import { ROUTES } from 'router/routes';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
+import { mockPosts } from 'shared/mocks/mockArticles';
 
 
 export const ArticlePage = () => {
     const {id} = useParams();
     const naigate = useNavigate();
     const location = useLocation();
+    const [searchValue, setsearchValue] = useState('');
+   
+
 
     const [params, setParams] = useSearchParams();
+    const onSearchChange = (e: ChangeEvent<HTMLInputElement>) => setsearchValue(e.target.value);
 
 
     console.log(location);
+    console.log(searchValue);
 
 
 
