fcaf9de76749c2063dd6997e75cce1ddfbb2868d
Yuliya <cudina.iu@yandex.ru>
2024-06-01 22:05:51 +0300

    ArticlePage

--- src/pages/ChampionshipsPage.tsx
+++ src/pages/ChampionshipsPage.tsx
@@ -6,19 +6,11 @@ import { mockPosts } from '../shared/mocks/mockArticles';
 
 export const ChampionshipsPage = () => {
   const [articles, setArticles] = useState(mockPosts);
-  // const [searchValue, setSearchValue] = useState('');
 
-  // const onSearchChange = (e: ChangeEvent<HTMLInputElement>) => setSearchValue(e.target.value);
 
-  const onSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const filteredArticles = mockPosts.filter(article =>
-      article.title.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase()),
-    );
-
-    return setArticles(filteredArticles);
-  };
 
   return (
+
       <div>
         <h2>Чемпионаты</h2> 
         
