fe54c2a90b92fe400cbc0592148f02d33e7fdd5f
Yuliya <cudina.iu@yandex.ru>
2024-06-10 00:24:57 +0300

    clearuserdata

--- src/features/page-wrapper/header/LoginButton.tsx
+++ src/features/page-wrapper/header/LoginButton.tsx
@@ -1,19 +1,30 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { getToken, getUserAvatar } from 'store/userData';
 import { ROUTES } from '../../../router/routes';
+import { useAppDispatch } from '../../../store';
+import { clearUserData, getToken, getUserAvatar } from '../../../store/userData';
+import { STORAGE_KEYS, clearStorageItem } from '../../..';
 import styles from './header.module.css';
 
+
 export const LoginButton = () => {
+  const dispatch = useAppDispatch();
   const token = useSelector(getToken);
   const avatar = useSelector(getUserAvatar);
+
+  const logoutHandler = () => {
+    dispatch(clearUserData());
+    clearStorageItem(STORAGE_KEYS.USER_DATA);
+  };
 
   if (token)
     return (
       <>
         {avatar && <img className={styles.avatar} src={avatar} alt="avatar" />}
 
-        {token && <button className={styles.newPostButton}>Выйти</button>}
+        {token && <button className={styles.newPostButton}>
+          Выйти
+        </button>}
       </>
     );
 
